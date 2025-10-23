f4ad4b3fc363e7644bcc79a647a30d7c5a1bb710
dgmetje1 <dgmetje1@gmail.com>
2025-10-24 00:32:25 +0200

    ci: configure host deploy

--- portfolio-app/astro.config.mjs
+++ portfolio-app/astro.config.mjs
@@ -8,7 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
-
+	site: 'https://dgmetje1.github.io',
+	base: '/portfolio',
 	vite: {
 		plugins: [tailwindcss()],
 		server: {
